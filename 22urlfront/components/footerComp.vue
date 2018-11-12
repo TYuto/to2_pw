679f3458acdfd7f14b769d7641a401c545ffbc25
YutoTamura <yuto.tamur@gmail.com>
2018-11-13 06:46:16 +0900

    [fix] footerのレイアウト

--- 22urlfront/components/footerComp.vue
+++ 22urlfront/components/footerComp.vue
@@ -1,27 +1,15 @@
 <template>
-  <div>
-    <footer class="footer">
-      <div class="container">
-        <p class="text-muted text-center">22url.link &copy;2018 YutoTamura</p>
-      </div>
-    </footer>
-  </div>
+  <footer
+    class="footer bg-light pt-3 pb-3">
+    <div class="">
+      <p class="text-muted text-center">22url.link &copy;2018 YutoTamura</p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
   .footer {
-    position: absolute;
-    bottom: 0;
     width: 100%;
     height: 60px;
-    background-color: #f5f5f5;
   }   
-  .container {
-    width: auto;
-    max-width: 680px;
-    padding: 0 15px;
-  }
-  .container .text-muted {
-     margin: 20px 0;
-  }
 </style>
--- 22urlfront/pages/index.vue
+++ 22urlfront/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
   <section>
-    <navbarComp />
-    <createUrl />
+    <section id="wraper">
+      <navbarComp />
+      <createUrl />
+      <urlTable />
+      <div id="push" />
+    </section>
     <footerComp />
   </section>
 </template>
@@ -10,14 +14,23 @@
 import navbarComp from '~/components/navbarComp.vue'
 import footerComp from '~/components/footerComp.vue'
 import createUrl from '~/components/createUrl.vue'
+import urlTable from '~/components/urlTable.vue'
 export default {
   components: {
     navbarComp,
     footerComp,
-    createUrl
+    createUrl,
+    urlTable
   }
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0
+}
+#wraper {
+  min-height: calc(100vh - 80px);
+}
 </style>
