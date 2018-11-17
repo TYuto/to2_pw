79d226b1bac33055766903a1cbcb3560f6225334
YutoTamura <s17130@tokyo.kosen-ac.jp>
2018-11-17 11:30:06 +0900

    [add] 色々

--- djangoserver/djangoserver/settings.py
+++ djangoserver/djangoserver/settings.py
@@ -72,11 +72,13 @@ WSGI_APPLICATION = 'djangoserver.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
