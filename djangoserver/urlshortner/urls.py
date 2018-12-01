2ee60241a0b56fabf4d383bad672785e4a091053
YutoTamura <yuto.tamur@gmail.com>
2018-12-01 11:23:16 +0900

    Revert "[add] https-portal導入"
    This reverts commit 59b9cf282d18c95448947312a9e1fc9baf160e5d.

--- djangoserver/urlshortner/urls.py
+++ djangoserver/urlshortner/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('urls/', views.urls.as_view()),
-    path('<str:domain>/<str:rand>', views.redirectView),
     path('<str:domain>/<str:rand>/', views.redirectView)
 ]
