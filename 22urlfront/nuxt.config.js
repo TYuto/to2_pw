7cbc6ad59370a124e0a4f27ae25cfdd22b1e9aeb
YutoTamura <yuto.tamur@gmail.com>
2018-12-01 18:33:31 +0900

    [add] frontのstatic file

--- 22urlfront/nuxt.config.js
+++ 22urlfront/nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/front/static/favicon.png' }
     ]
   },
 
