21923a42d501dcc4740e233cb61d6d485a24028a
Yuto Tamura <yuto.tamur@gmail.com>
2019-08-11 16:24:42 +0900

    [fix] プライバシーポリシー

--- 22urlfront/components/md/privacyPolicy.vue
+++ 22urlfront/components/md/privacyPolicy.vue
@@ -7,13 +7,14 @@
 
 当サイトではアクセス解析のため Google Analyticsを利用してサイトのアクセス解析を行っております。Google Analyticsはトラフィックデータの収集のためCookieを利用しています、Cookieはブラウザの設定で無効化することができます。サイト利用者は当サイトを利用することでGoogle Analyticsによる情報集収に同意したこととみなします。
 
-[Google アナリティクス利用規約](https://marketingplatform.google.com/about/analytics/terms/jp/)
 [Googleプライバシーポリシー](https://policies.google.com/privacy?hl=ja)
+[Google 利用規約](https://policies.google.com/terms)
 ### reCAPTCHA
 
 当サイトではボットによる不正アクセス防止のため、Googleが提供しているreCAPTCHAを利用しています。reCAPTCHAはIPアドレスなどの情報を収集し、それらのデータを分析のために Google へ送信します、サイト利用者は当サイトを利用することでreCAPTCHAの情報収集に同意したこととみなします。
 
 [Googleプライバシーポリシー](https://policies.google.com/privacy?hl=ja)
+[Google 利用規約](https://policies.google.com/terms)
 
 ### プライバシーポリシーの変更について
 
